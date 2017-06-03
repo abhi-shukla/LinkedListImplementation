2d1963f2bf64a2f539855336ee2ef6f4b1a026ff
Abhishek Shukla <abhishekshukla85@hotmail.com>
2017-06-03 10:35:25 -0700

    Implement linked list data structure
     - Create Node class
    
     - Create LinkedList class
    
     - Add functionality to add node to the front and end of the linked list

---
+++ LinkedListApp/MyLinkedList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListApp
+{
+    public class MyLinkedList
+    {
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+
+        public void AddToFront(Node node)
+        {
+            if(node != null)
+            {
+                if (Head == null && Tail == null)
+                {
+                    Head = node;
+                    Tail = node;
+                }
+                else
+                {
+                    var temp = Head;
+                    Head = node;
+                    Head.Next = temp;
+                }
+
+            }
+        }
+
+        public void AddToEnd(Node node)
+        {
+            if(node != null)
+            {
+                if(Head == null && Tail == null)
+                {
+                    Head = node;
+                    Tail = node;
+                }
+                else
+                {
+                    Tail.Next = node;
+                    Tail = node;
+                }
+            }
+        }
+    }
+}
--- LinkedListApp/Program.cs
+++ LinkedListApp/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LinkedListApp
 {
@@ -10,6 +6,48 @@ namespace LinkedListApp
     {
         static void Main(string[] args)
         {
+            var first = new Node { Value = 3 };
+            var second = new Node { Value = 5 };
+            //first.Next = second;
+            var third = new Node { Value = 7 };
+            //second.Next = third;
+            //PrintNodes(first); 
+
+            var linkedList = new MyLinkedList();
+            linkedList.AddToFront(third);
+            linkedList.AddToFront(second);
+            linkedList.AddToFront(first);
+
+            //PrintLinkedList(linkedList);
+
+            var otherLinkedList = new MyLinkedList();
+            otherLinkedList.AddToEnd(first);
+            otherLinkedList.AddToEnd(second);
+            otherLinkedList.AddToEnd(third);
+
+            PrintLinkedList(otherLinkedList);
+        }
+
+        private static void PrintNodes(Node node)
+        {            
+            while (node != null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;                
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void PrintLinkedList(MyLinkedList linkedList)
+        {
+            while (linkedList.Head != null)
+            {
+                Console.WriteLine(linkedList.Head.Value);
+                linkedList.Head = linkedList.Head.Next;
+            }
+
+            Console.ReadLine();
         }
     }
 }
